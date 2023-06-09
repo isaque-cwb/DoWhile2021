84a00c72fc514e4e0a1be95349ba8c74dfabc1e4
Isaque Lourenço <isaque.lourenco2@gmail.com>
2023-06-08 22:51:13 -0300

    Autenticação do Usuáriocom Token JWT

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,14 +1,16 @@
 
 /**
- * Receber code(string)
- * Recuperar o access_token no github
- * Recuperar infos do iser no github
+ * Receber code(string) = Redirecionar o Usuário para autorizar o git
+ * Recuperar o access_token no github = enviar credenciais do app(id e secret) para git com codigo recebido recebe token do git
+ * Recuperar infos do user no github = com o token acessar api e obter dados
  * Verificar se o usuário existe no DB
  * --- SIM = Gera um Token
  * --- NAO = Cria no DB, Gera um token 
  * Retornar o Token com as infos do user
  */
 import axios from "axios"
+import prismaClient from "../prisma"
+import { sign } from "jsonwebtoken"
 
 interface IAccesTokenResponse {
     access_token: string
@@ -42,7 +44,39 @@ class AuthenticateUserService {
         })
 
 
-        return response.data
+        const { login, id, avatar_url, name } = response.data
+
+        let user = await prismaClient.user.findFirst({
+            where: {
+                github_id: id
+            }
+        })
+
+        if (!user) {
+            user = await prismaClient.user.create({
+                data: {
+                    github_id: id,
+                    login,
+                    avatar_url,
+                    name
+                }
+            })
+        }
+
+        const token = sign({
+            user: {
+                github_id: user.github_id,
+                avatar_url: user.avatar_url,
+                id: user.id,
+            }
+        }, process.env.JWT_SECRET,
+            {
+                subject: user.id,
+                expiresIn: '1H'
+            }
+        )
+
+        return { token, user }
 
     }
 }
