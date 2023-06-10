da0df2cd24c39b4a5aa9fb4ef04b54a0d6e6caf3
Isaque Lourenço <isaque.lourenco2@gmail.com>
2023-06-10 19:23:16 -0300

    Adicionado rota last 3 messages

--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,13 @@ import { Router } from 'express'
 import { AuthenticateUserController } from './controllers/AuthenticateUserController'
 import { CreateMessageController } from './controllers/CreateMessageController'
 import { ensureAuthenticated } from './middleware/ensureAuthenticated'
+import { GetLast3MessageController } from './controllers/GetLast3MessageController'
 
 const router = Router()
 
 router.post("/authenticate", new AuthenticateUserController().handle)
 router.post("/messages", ensureAuthenticated, new CreateMessageController().handle)
+router.get("/messages/last3", new GetLast3MessageController().handle)
 
 
 export { router }
