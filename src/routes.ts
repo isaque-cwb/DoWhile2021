d1251e0e1827dda18e3f3894fa25510a1582f32c
Isaque Lourenço <isaque.lourenco2@gmail.com>
2023-06-10 19:40:15 -0300

    Rota Busca Profile

--- src/routes.ts
+++ src/routes.ts
@@ -3,12 +3,14 @@ import { AuthenticateUserController } from './controllers/AuthenticateUserContro
 import { CreateMessageController } from './controllers/CreateMessageController'
 import { ensureAuthenticated } from './middleware/ensureAuthenticated'
 import { GetLast3MessageController } from './controllers/GetLast3MessageController'
+import { ProfileUserController } from './controllers/ProfileUserController'
 
 const router = Router()
 
 router.post("/authenticate", new AuthenticateUserController().handle)
 router.post("/messages", ensureAuthenticated, new CreateMessageController().handle)
 router.get("/messages/last3", new GetLast3MessageController().handle)
+router.get("/profile", ensureAuthenticated, new ProfileUserController().handle)
 
 
 export { router }
