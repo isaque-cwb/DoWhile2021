6a09ebe2ee1f6cf4a604aaa4f348ef28dc35b510
Isaque Lourenço <isaque.lourenco2@gmail.com>
2023-06-12 23:42:30 -0300

    Login e Autenticação JWT e contextAPI criados

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
 import './styles/global.css'
+import { AuthProvider } from './contexts/auth.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <AuthProvider>
     <App />
-  </React.StrictMode>,
+  </AuthProvider>
 )
