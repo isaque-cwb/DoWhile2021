234de8083bf0d0359038ad96a1298b69cf49d2f5
isaque-cwb <isaque.lourenco2@gmail.com>
2023-06-14 17:01:10 -0300

    Componente SenMessageForm criado

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import { useContext } from 'react'
 import styles from './App.module.scss'
 import { LoginBox } from './LoginBox'
 import { MessageList } from './MessageList'
+import { AuthContext } from './contexts/auth'
+import { SendMessageForm } from './SendMessageForm'
 
 
 export function App() {
+
+  const { user } = useContext(AuthContext)
   return (
     <main className={styles.contentWrapper}>
       <MessageList />
-      <LoginBox />
+      {!!user ? <SendMessageForm/> : <LoginBox />}
     </main>
   )
 }
--- src/LoginBox/index.tsx
+++ src/LoginBox/index.tsx
@@ -7,9 +7,8 @@ import { AuthContext } from '../contexts/auth';
 
 export function LoginBox() {
 
-    const { signInUrl, user } = useContext(AuthContext)
+    const { signInUrl } = useContext(AuthContext) 
 
-    console.log(user)
     return (
         <div className={styles.loginBoxWrapper}>
             <strong>Entre e compartilhe sua mensagem</strong>
